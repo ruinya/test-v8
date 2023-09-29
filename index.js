20f95eb6e57c8392e95dbf5338acb59881831241
ShoZYXrk <mistershok11239@gmail.com>
2023-09-29 10:34:54 +0300

    step 1 fin

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+import _ from 'lodash';
 
+export default function showInfo(content) {
+
+    const data = content.trim().split('\n').slice(1).map((row) => row.split(',').slice(0));;
+    // console.log(data);
+    
+    // test 1
+    console.log(`Coun: ${data.length}`);
+}

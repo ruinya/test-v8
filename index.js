fffb1eab0cced19594ea7adca049e31c5d695365
ShoZYXrk <mistershok11239@gmail.com>
2023-09-29 10:48:37 +0300

    step 2, 3 - fin

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// import _ from 'lodash';
+import _ from 'lodash';
 
 export default function showInfo(content) {
   const data = content.trim().split('\n').slice(1).map((row) => row.split(',').slice(0));
@@ -7,5 +7,17 @@ export default function showInfo(content) {
   // test 1
   console.log(`Coun: ${data.length}`);
 
-  // step
+  // step 2
+  const barberShops = data.map(((shops) => shops[0]));
+  const sortBarberShops = _.sortBy(barberShops);
+
+  console.log(`Barbershops: ${(sortBarberShops).join(', ')}`);
+
+  // step 3
+
+  const minRate = _.minBy(data.map((shop) => shop[4]));
+  const maxRate = _.maxBy(data.map((shop) => shop[4]));
+
+  console.log(`Ratings: Min: ${minRate} Max: ${maxRate}`);
+
 }

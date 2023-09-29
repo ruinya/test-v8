41bb768a1e8535412aa04e4fc8a031413566ad83
ShoZYXrk <mistershok11239@gmail.com>
2023-09-29 10:52:38 +0300

    step 4 - fin

--- index.js
+++ index.js
@@ -19,4 +19,12 @@ export default function showInfo(content) {
   const maxRate = _.maxBy(data.map((shop) => shop[4]));
 
   console.log(`Ratings: Min: ${minRate} Max: ${maxRate}`);
+
+  // step 4
+
+  const yearsLive = _.maxBy(data, (year) => Number(year[3]));
+
+  const maxLiveYears = yearsLive[0];
+
+  console.log(`Oldest barbershop: ${maxLiveYears}`);
 }
